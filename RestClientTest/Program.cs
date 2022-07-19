ffdafe86c41e55c28b74a2d44c45934767bcb697
Guilherme Rocha <guifgr@hotmail.com>
2022-07-19 10:19:38 -0300

    Benchmark

---
+++ RestClientTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using RestClientTest;
+
+Console.WriteLine("Benchmark RestSharp Vs HttpClient");
+
+var config =  new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+BenchmarkRunner.Run<RestCalls>(config);
---
+++ RestClientTest/RestCalls.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using RestSharp;
+
+namespace RestClientTest;
+
+[MemoryDiagnoser(true)]
+[HtmlExporter]
+public class RestCalls
+{
+  private const string Url = "INSERT SOME API URL HERE";
+  // I recommend .net minimal Apis
+
+  [Benchmark]
+  public void RestSharpCall()
+  {
+    var client = new RestClient();
+    var request = new RestRequest(Url);
+    client.Execute(request);
+  }
+
+  [Benchmark]
+  public void DotNetHttpCall()
+  {
+    var httpClient = new HttpClient();
+    httpClient.GetAsync(Url);
+  }
+}
